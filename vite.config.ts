f4ec52024019920e39fdafcae694fce0a66b3088
gregtory03-lab <vidempershinkstra@gmail.com>
2026-05-15 09:53:21 +0300

    recommit: update package and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tsConfigPaths(), tanstackStart(), react()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), react()],
 });
